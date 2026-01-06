0f0c53a45cf2188f17d77eed24644e57816c42db
Sjoerd <sjoerd.hoogvliet@student.hu.nl>
2026-01-06 21:05:55 +0100

    P5H: Changing OVChipkaart files where necessary

--- src/main/java/ovchipkaart/OVChipkaart.java
+++ src/main/java/ovchipkaart/OVChipkaart.java
@@ -1,6 +1,10 @@
 package ovchipkaart;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Hibernate;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -8,7 +12,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import ovchipkaartproduct.OVChipkaartProduct;
+import product.Product;
 import reiziger.Reiziger;
 
 @Entity
@@ -24,6 +31,8 @@ public class OVChipkaart {
     @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "reiziger_id", nullable = false)
     private Reiziger reiziger;
+    @OneToMany(mappedBy = "ovChipkaart", orphanRemoval = true)
+    private List<OVChipkaartProduct> productRelaties = new ArrayList<>();
 
     // JPA required empty constructor
     public OVChipkaart() {}
@@ -36,13 +45,43 @@ public class OVChipkaart {
         this.reiziger = reiziger;
     }
 
+    // TODO: Add something that gives some information about the products
     public String toString() {
-        return "OV Chipkaart #%d: met %.2f euro is geldig tot %s in klasse %d".formatted(
+        String returnString = "OV Chipkaart #%d: met %.2f euro is geldig tot %s in klasse %d".formatted(
             this.kaartNummer,
             this.saldo,
             this.geldigTot,
             this.klasse
         );
+
+        if(Hibernate.isInitialized(this.productRelaties) && this.productRelaties.size() > 0) {
+            returnString += "\n\tHeeft de volgende producten: ";
+            for(OVChipkaartProduct p : this.productRelaties) {
+                returnString += p.getProduct().getProductNummer() + " ";
+            }
+        }
+
+        return returnString;
+    }
+
+    public void addProduct(Product product) {
+        for(OVChipkaartProduct p : this.productRelaties) {
+            if(p.getProduct().getProductNummer() == product.getProductNummer()) {
+                return;
+            }
+        }
+        this.productRelaties.add(new OVChipkaartProduct(this, product));
+    }
+
+    public void removeProduct(Product product) {
+        productRelaties.removeIf(okp -> {
+            boolean match = okp.getProduct().equals(product);
+            if (match) {
+                okp.getProduct().getOVChipkaartRelaties().remove(okp);
+                this.productRelaties.remove(okp);
+            }
+            return match;
+        });
     }
 
     //*** Get/Set ***//
@@ -84,5 +123,13 @@ public class OVChipkaart {
 
     public void setReiziger(Reiziger reiziger) {
         this.reiziger = reiziger;
+    }
+
+    public List<OVChipkaartProduct> getProductRelaties() {
+        return productRelaties;
+    }
+
+    public void setProductRelaties(List<OVChipkaartProduct> productRelaties) {
+        this.productRelaties = productRelaties;
     }
 }
--- src/main/java/ovchipkaart/OVChipkaartDAOHibernate.java
+++ src/main/java/ovchipkaart/OVChipkaartDAOHibernate.java
@@ -2,10 +2,12 @@ package ovchipkaart;
 
 import java.util.List;
 
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import ovchipkaartproduct.OVChipkaartProduct;
 import reiziger.Reiziger;
 
 public class OVChipkaartDAOHibernate implements OVChipkaartDAO {
@@ -33,12 +35,27 @@ public class OVChipkaartDAOHibernate implements OVChipkaartDAO {
         }
     }
 
+    // Update functie waar iets fout gaat met merge wat een update functie moet zijn (hij denkt nu nieuw te moeten inserten)
     @Override
     public boolean update(OVChipkaart ovchipkaart) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
         try {
-            session.merge(ovchipkaart);
+            // Updating method for managed entities, as merge tried to insert on some occasions and we're using Hibernate 7.x
+            OVChipkaart existingOVChipkaart = session.find(OVChipkaart.class, ovchipkaart.getKaartNummer());
+            existingOVChipkaart.setSaldo(ovchipkaart.getSaldo());
+            existingOVChipkaart.setGeldigTot(ovchipkaart.getGeldigTot());
+            existingOVChipkaart.setKlasse(ovchipkaart.getKlasse());
+            existingOVChipkaart.setReiziger(ovchipkaart.getReiziger());
+            existingOVChipkaart.getProductRelaties().clear();
+            
+            List<OVChipkaartProduct> relations = session.createQuery("select relation from OVChipkaartProduct relation where relation.ovChipkaart = :ovChipkaart", OVChipkaartProduct.class)
+                .setParameter("ovChipkaart", existingOVChipkaart)
+                .getResultList();
+
+            for (OVChipkaartProduct r : relations) {
+                existingOVChipkaart.addProduct(r.getProduct());
+            }
             transaction.commit();
             session.close();
             return true;
@@ -74,6 +91,9 @@ public class OVChipkaartDAOHibernate implements OVChipkaartDAO {
         Session session = sessionFactory.openSession();
         try {
             OVChipkaart ovchipkaart = session.find(OVChipkaart.class, kaartNummer);
+
+            Hibernate.initialize(ovchipkaart.getProductRelaties());
+
             session.close();
             return ovchipkaart;
         } catch (Exception e) {
@@ -92,6 +112,10 @@ public class OVChipkaartDAOHibernate implements OVChipkaartDAO {
                 .setParameter("reiziger", reiziger)
                 .getResultList();
 
+            for (OVChipkaart o : ovchipkaarten) {
+                Hibernate.initialize(o.getProductRelaties());
+            }
+
             session.close();
             return ovchipkaarten;
         } catch (Exception e) {
@@ -107,6 +131,11 @@ public class OVChipkaartDAOHibernate implements OVChipkaartDAO {
         Session session = sessionFactory.openSession();
         try {
             List<OVChipkaart> ovchipkaarten = session.createQuery("from OVChipkaart", OVChipkaart.class).getResultList();
+
+            for (OVChipkaart o : ovchipkaarten) {
+                Hibernate.initialize(o.getProductRelaties());
+            }
+
             session.close();
             return ovchipkaarten;
         } catch (Exception e) {
